6fd774eb1816f97d854f2e88dee5697a5d7ce8ae
conectis360 <conectis360@gmail.com>
2020-02-25 17:10:53 -0300

    adding better routes to the projects

--- serve_html/main.js
+++ serve_html/main.js
@@ -1,59 +1,38 @@
 const port = 3000,
     http = require("http"),
     httpStatus = require("http-status-codes"),
+    router = require("./router")
     fs = require('fs');
-
-const sendErrorResponse = res => {
-    res.writeHead(httpStatus.NOT_FOUND, {
-        "Content-Type": "text/html"
-    });
-    res.write("<h1>File Not Found!</h1>");
-    res.end();
-};
-
-http
-    .createServer((req, res) => {
-        let url = req.url;
-        if(url.indexOf(".html") !== -1) {
-            res.writeHead(httpStatus.OK, {
-                "Content-Type": "text/html"
-            });
-            customReadFile(`./views${url}`, res);
-        }else if (url.indexOf(".js") !== -1) {
-            res.writeHead(httpStatus.OK, {
-                "Content-Type": "text/javascript"
-            });
-            customReadFile(`./public/js${url}`, res);
-        }else if (url.indexOf(".css") !== -1) {
-            res.writeHead(httpStatus.OK, {
-                "Content-Type": "text/css"
-            });
-            customReadFile(`./public/css${url}`, res);
-        }else if (url.indexOf(".png") !== -1) {
-            res.writeHead(httpStatus.OK, {
-                "Content-Type": "image.png"
-            });
-            customReadFile(`./public/images${url}`, res);
-        }else {
-            sendErrorResponse(res);
-        }
-
-    })
-    .listen(port);
-console.log(`Server up na porta: ${port}`);
-
-const customReadFile = (file_path, res) => {
-    if (fs.existsSync(file_path)) {
-        fs.readFile(file_path, (error, data) => {
-            if(error) {
-                console.log(error);
-                sendErrorResponse(res);
-                return;
+    plainTextContentType = {
+        "Content-type": "text/plain"
+    },
+    htmlContentType = {
+        "Content-type": "text/html"
+    };
+    customReadFile = (file, res) => {
+        fs.readFile(`./${file}`, (errors, data) => {
+            if(errors) {
+                console.log("Erro ao ler o arquivo..");
             }
-            res.write(data);
-            res.end();
+            res.end(data);
         });
-    }else{
-        sendErrorResponse(res);
-    }
-};
+    };
+
+    router.get("/", (req, res) => {
+        res.writeHead(httpStatus.OK, plainTextContentType);
+        res.end("index");
+    });
+    
+    router.get("/index.html", (req, res) => {
+        res.writeHead(httpStatus.OK, htmlContentType);
+        customReadFile("views/index.html", res);
+    });
+
+    router.post("/", (req, res) => {
+        res.writeHead(httpStatus.OK, plainTextContentType);
+        res.end("postado.");
+    });
+
+
+http.createServer(router.handle).listen(port);
+console.log(`Servidor está ouvindo a porta: ${port}`);
