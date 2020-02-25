82e83b0d5c5eae96c34f268b25750ef3d2ead31a
conectis360 <conectis360@gmail.com>
2020-02-25 16:08:15 -0300

    making of routes to servehtml

--- serve_html/main.js
+++ serve_html/main.js
@@ -1,23 +1,59 @@
 const port = 3000,
     http = require("http"),
-    httpStatus = require("http-status-code"),
-    fs = require('fs'),
-    const routeMap = {
-        '/' : 'views/index.html'
-    };
-    http
-        .createServer((req, res) => {
+    httpStatus = require("http-status-codes"),
+    fs = require('fs');
+
+const sendErrorResponse = res => {
+    res.writeHead(httpStatus.NOT_FOUND, {
+        "Content-Type": "text/html"
+    });
+    res.write("<h1>File Not Found!</h1>");
+    res.end();
+};
+
+http
+    .createServer((req, res) => {
+        let url = req.url;
+        if(url.indexOf(".html") !== -1) {
             res.writeHead(httpStatus.OK, {
-                "Content-Type" : "text/html"
+                "Content-Type": "text/html"
             });
-            if(routeMap[req.url]){
-                fs.readFile(routeMap[req.url], (error, data) => {
-                    res.write();
-                    res.end();
-                });
-            }else{
-                res.end("<h1> Sorry, not found</h1>");
+            customReadFile(`./views${url}`, res);
+        }else if (url.indexOf(".js") !== -1) {
+            res.writeHead(httpStatus.OK, {
+                "Content-Type": "text/javascript"
+            });
+            customReadFile(`./public/js${url}`, res);
+        }else if (url.indexOf(".css") !== -1) {
+            res.writeHead(httpStatus.OK, {
+                "Content-Type": "text/css"
+            });
+            customReadFile(`./public/css${url}`, res);
+        }else if (url.indexOf(".png") !== -1) {
+            res.writeHead(httpStatus.OK, {
+                "Content-Type": "image.png"
+            });
+            customReadFile(`./public/images${url}`, res);
+        }else {
+            sendErrorResponse(res);
+        }
+
+    })
+    .listen(port);
+console.log(`Server up na porta: ${port}`);
+
+const customReadFile = (file_path, res) => {
+    if (fs.existsSync(file_path)) {
+        fs.readFile(file_path, (error, data) => {
+            if(error) {
+                console.log(error);
+                sendErrorResponse(res);
+                return;
             }
-        })
-        .listen(port);
-        console.log("Server up na porta: " + port);
+            res.write(data);
+            res.end();
+        });
+    }else{
+        sendErrorResponse(res);
+    }
+};
