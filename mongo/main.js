ecf256cbae2820e604e363fcf6a8322908c17468
conectis360 <conectis360@gmail.com>
2020-02-29 00:37:56 -0300

    using moongose

--- mongo/main.js
+++ mongo/main.js
@@ -1,20 +1,43 @@
-const mongoDB = require('mongodb').MongoClient,
-    dbURL = "mongodb://localhost:27017",
-    dbName = "recipe_db";
+const mongoose = require("mongoose");
+mongoose.connect(
+    "mongodb://localhost:27017/recipe_db",
+    {useNewUrlParser: true}
+);
+const db = mongoose.connection;
+db.once("open", () => {
+    console.log("Sucesso em conectar com o MongoDb via Mongoose");
+});
+Subscriber = require("./models/subscriber");
+var subscriber1 = new Subscriber({
+    name: "Jon Wexler",
+    email: "jon@jonwexler.com"
+});
+
+subscriber1.save((error, savedDocument) => {
+    if(error) console.log(error);
+    console.log(savedDocument);
+});
+Subscriber.create(
+    {
+        name: "Jon Wexler",
+        email: "jon@jonwexler.com"
+    },
+    function (error, savedDocument) {
+        if(error) console.log(error);
+        console.log(savedDocument);
+    }
+);
+
 const express = require('express');
     app = express();
 
-mongoDB.connect(dbURL, (error, client) => {
-    if (error) throw error;
-    let db = client.db(dbName);
-    db.collection("contacts")
-      .find()
-      .toArray((error, data) => {
-          if(error) throw error;
-          console.log(data);
-      });
-});
+var myQuery = Subscriber.findOne({
+    name: "Jon Wexler"
+}).where("email", /wexler/);
 
+myQuery.exec((error, data) => {
+    if (data) console.log(data.name);
+});
 
 app.listen(3000, () => {
     console.log(`Server na porta: 3000`);
