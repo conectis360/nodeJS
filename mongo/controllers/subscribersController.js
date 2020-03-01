83b7ff8124e540806a724e3e81eaf2d7c588815c
conectis360 <conectis360@gmail.com>
2020-03-01 14:55:23 -0300

    working on datas with mongo

---
+++ mongo/controllers/subscribersController.js
@@ -0,0 +1,29 @@
+const Subscriber = require("../models/subscriber");
+
+exports.getAllSubscribers = (req, res, next) => {
+  Subscriber.find({}, (error, subscribers) => {
+    if (error) next(error);
+    req.data = subscribers;
+    next();
+  });
+};
+
+exports.getSubscriptionPage = (req, res) => {
+  res.render("contact");
+};
+
+exports.saveSubscriber = (req, res) => {
+    let newSubscriber = new Subscriber({
+      name: req.body.name,
+      email: req.body.email,
+      zipCode: req.body.zipCode
+    });
+    newSubscriber
+      .save()
+      .then(result => {
+        res.render("thanks");
+      })
+      .catch(error => {
+        if (error) res.send(error);
+      });
+  };
